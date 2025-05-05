bcff63087a5a2f8160f0ad541ab81fd6db781800
Suvam_Chanda <ssuvam08@gmail.com>
2025-05-05 09:15:52 +0530

    Update JS

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-var greeting = 'Hello, World!';
+var greetingMessage = 'Hello, World TS!';
+console.log(greetingMessage);
 // create a new heading 1 element
 var heading = document.createElement('h1');
-heading.textContent = greeting;
+heading.textContent = greetingMessage;
 // add the heading the document
 document.body.appendChild(heading);
-function getName() {
-    return { name: 'Suvam Chanda', age: 30 };
-}
