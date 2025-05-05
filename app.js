4169338b24411017670be942679bcfeed72f8644
Suvam_Chanda <ssuvam08@gmail.com>
2025-05-05 09:13:57 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var greeting = 'Hello, World!';
+// create a new heading 1 element
+var heading = document.createElement('h1');
+heading.textContent = greeting;
+// add the heading the document
+document.body.appendChild(heading);
+function getName() {
+    return { name: 'Suvam Chanda', age: 30 };
+}
---
+++ app.ts
@@ -0,0 +1,12 @@
+let greetingMessage: string = 'Hello, World TS!';
+console.log(greetingMessage);
+
+// create a new heading 1 element
+let heading = document.createElement('h1');
+heading.textContent = greetingMessage;
+
+// add the heading the document
+document.body.appendChild(heading);
+
+
+  
